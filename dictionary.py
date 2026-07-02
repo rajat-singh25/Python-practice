7be25d8824da64669b045c92fd076c0aaa4d7fb6
Rajat Kumar Singh <rajat2002s@gmail.com>
2026-07-02 11:13:43 +0530

    Add student dictionary and print details

---
+++ dictionary.py
@@ -0,0 +1,13 @@
+student = {
+    "Name": "Rajat",
+    "Age": 24,
+    "Course": "MCA"
+}
+
+print(student)
+
+print("Name:", student["Name"])
+
+student["College"] = "Lovely Professional University"
+
+print(student)
